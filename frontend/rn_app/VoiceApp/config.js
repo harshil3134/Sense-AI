5f991bd0d7c507c3f25a25c8172a765664181f2b
harshil3134 <harshilpatel2223@gmail.com>
2025-10-03 17:45:05 +0530

    minor change in config file

--- frontend/rn_app/VoiceApp/config.js
+++ frontend/rn_app/VoiceApp/config.js
@@ -1,5 +1,5 @@
 // Get the API URL from environment or use a default
-export const API_URL ='http://192.168.1.200:8000' // Production
+export const API_URL =process.env.EXPO_PUBLIC_API_URL // Production
 
 // Optional: Export other config values
 export const API_TIMEOUT = 30000; // 30 seconds
